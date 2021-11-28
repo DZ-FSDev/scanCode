d755b6d72ef96ff2427546cf30d969d196dbe3a8
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-11-28 10:33:18 -0600

    Update UPCGen.java

--- upc/UPCGen.java
+++ upc/UPCGen.java
@@ -1,32 +1,36 @@
 package com.dz_fs_dev.scanCode.upc;
 
-import net.sourceforge.barbecue.Barcode;
-import net.sourceforge.barbecue.BarcodeException;
-import net.sourceforge.barbecue.BarcodeFactory;
-import net.sourceforge.barbecue.BarcodeImageHandler;
-import net.sourceforge.barbecue.output.OutputException;
-
-import java.awt.Font;
 import java.awt.image.BufferedImage;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 /**
  * Contains various methods for processing UPCs.
  * 
  * @author DZ_FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 public final class UPCGen {
 	private UPCGen() {}
 	
-	public static BufferedImage generateEAN13BarcodeImage(String barcodeText) throws BarcodeException, OutputException{
-	    return generateEAN13BarcodeImage(barcodeText, new Font("Arial", 0, 16));
+	public static BufferedImage generateEAN13BarcodeImage(String barcodeText) throws WriterException {
+		EAN13Writer ean13BarcodeWriter = new EAN13Writer();
+	    BitMatrix bitMatrix = ean13BarcodeWriter.encode(barcodeText, BarcodeFormat.EAN_13, 300, 150);
+
+	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
 	}
 	
-	public static BufferedImage generateEAN13BarcodeImage(String barcodeText, Font font) throws BarcodeException, OutputException{
-	    Barcode barcode = BarcodeFactory.createEAN13(barcodeText);
-	    barcode.setFont(font);
+	public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
+	    QRCodeWriter qrBarcodeWriter = new QRCodeWriter();
+	    BitMatrix bitMatrix = 
+	    		qrBarcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
 
-	    return BarcodeImageHandler.getImage(barcode);
+	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
 	}
 }
