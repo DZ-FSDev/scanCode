a9b733c82a75f55886b7fef40898cd5f01419d9a
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2022-01-03 12:46:20 -0600

    Update and rename UPCGen.java to ScanCodeTools.java

--- upc/ScanCodeTools.java
+++ upc/ScanCodeTools.java
@@ -1,3 +1,21 @@
+/*  Original Licensing Copyright
+ * 
+ *  Various methods for processing UPCs.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.dz_fs_dev.scanCode.upc;
 
 import java.awt.image.BufferedImage;
@@ -8,7 +26,9 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
 import com.google.zxing.oned.Code39Writer;
+import com.google.zxing.oned.Code93Writer;
 import com.google.zxing.oned.EAN13Writer;
 import com.google.zxing.oned.EAN8Writer;
 import com.google.zxing.oned.UPCAWriter;
@@ -22,14 +42,14 @@ import com.dz_fs_dev.common.Graphics2DTools;
  * 
  * @author DZ_FSDev
  * @since 17.0.1
- * @version 0.0.8
+ * @version 0.0.10
  */
-public final class UPCGen {
+public final class ScanCodeTools {
 	private static int WIDTH_1D = 300;
 	private static int HEIGHT_1D = 150;
 
-	private UPCGen() {}
-	
+	private ScanCodeTools() {}
+
 	/**
 	 * Generates an EAN8 Barcode for a specified text to encode.
 	 * 
@@ -43,6 +63,19 @@ public final class UPCGen {
 	    BitMatrix bitMatrix = ean8BarcodeWriter.encode(barcodeText, BarcodeFormat.EAN_8, WIDTH_1D, HEIGHT_1D);
 
 	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
+	}
+	
+	/**
+	 * Generates an EAN13 Barcode for a specified text to encode and saves it to a file.
+	 * 
+	 * @param barcodeText The text to encode.
+	 * @param file The file to save the EAN13 barcode to as a PNG.
+	 * @throws WriterException
+	 * @throws IOException
+	 * @since 0.0.3
+	 */
+	public static void generateEAN13BarcodeImage(String barcodeText, File file) throws WriterException, IOException {
+		Graphics2DTools.saveAsPNG(generateEAN13BarcodeImage(barcodeText), file);
 	}
 	
 	/**
@@ -99,8 +132,8 @@ public final class UPCGen {
 	 * @since 0.0.7
 	 */
 	public static BufferedImage generateCode39BarcodeImage(String barcodeText) throws WriterException{
-		Code39Writer cod39BarcodeWriter = new Code39Writer();
-	    BitMatrix bitMatrix = cod39BarcodeWriter.encode(barcodeText, BarcodeFormat.CODE_39, WIDTH_1D, HEIGHT_1D);
+		Code39Writer code39BarcodeWriter = new Code39Writer();
+	    BitMatrix bitMatrix = code39BarcodeWriter.encode(barcodeText, BarcodeFormat.CODE_39, WIDTH_1D, HEIGHT_1D);
 
 	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
 	}
@@ -111,26 +144,28 @@ public final class UPCGen {
 	 * @param barcodeText The text to encode.
 	 * @return The Code93 Barcode.
 	 * @throws WriterException
-	 * @since 0.0.8
+	 * @since 0.0.9
 	 */
 	public static BufferedImage generateCode93BarcodeImage(String barcodeText) throws WriterException{
-		Code39Writer cod93BarcodeWriter = new Code39Writer();
-	    BitMatrix bitMatrix = cod93BarcodeWriter.encode(barcodeText, BarcodeFormat.CODE_93, WIDTH_1D, HEIGHT_1D);
+		Code93Writer code93BarcodeWriter = new Code93Writer();
+	    BitMatrix bitMatrix = code93BarcodeWriter.encode(barcodeText, BarcodeFormat.CODE_93, WIDTH_1D, HEIGHT_1D);
 
 	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
 	}
 	
 	/**
-	 * Generates an EAN13 Barcode for a specified text to encode and saves it to a file.
+	 * Generates an Code128 Barcode for a specified text to encode.
 	 * 
 	 * @param barcodeText The text to encode.
-	 * @param file The file to save the EAN13 barcode to as a PNG.
+	 * @return The Code128 Barcode.
 	 * @throws WriterException
-	 * @throws IOException
-	 * @since 0.0.3
+	 * @since 0.0.10
 	 */
-	public static void generateEAN13BarcodeImage(String barcodeText, File file) throws WriterException, IOException {
-		Graphics2DTools.saveAsPNG(generateEAN13BarcodeImage(barcodeText), file);
+	public static BufferedImage generateCode128BarcodeImage(String barcodeText) throws WriterException{
+		Code128Writer code128BarcodeWriter = new Code128Writer();
+	    BitMatrix bitMatrix = code128BarcodeWriter.encode(barcodeText, BarcodeFormat.CODE_128, WIDTH_1D, HEIGHT_1D);
+
+	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
 	}
 	
 	/**
@@ -147,5 +182,33 @@ public final class UPCGen {
 	    		qrBarcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
 
 	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
+	}
+	
+	/**
+	 * @return the wIDTH_1D
+	 */
+	public static int getWIDTH_1D() {
+		return ScanCodeTools.WIDTH_1D;
+	}
+
+	/**
+	 * @param wIDTH_1D the wIDTH_1D to set
+	 */
+	public static void setWIDTH_1D(int wIDTH_1D) {
+		ScanCodeTools.WIDTH_1D = wIDTH_1D;
+	}
+
+	/**
+	 * @return the hEIGHT_1D
+	 */
+	public static int getHEIGHT_1D() {
+		return ScanCodeTools.HEIGHT_1D;
+	}
+
+	/**
+	 * @param hEIGHT_1D the hEIGHT_1D to set
+	 */
+	public static void setHEIGHT_1D(int hEIGHT_1D) {
+		ScanCodeTools.HEIGHT_1D = hEIGHT_1D;
 	}
 }
